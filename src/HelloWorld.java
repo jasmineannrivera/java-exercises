24248a9b71727d6137869682bf627b999cf9062c
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-11-24 15:00:57 -0600

    test class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+        String myString = "Mira";
+        System.out.println(myString);
+
+
     }
 }
