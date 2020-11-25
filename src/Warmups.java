5bc5c08334109458d000c1a28c6beed9fc777ac3
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-11-25 11:54:19 -0600

    scanner test

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class Warmups {
@@ -16,8 +18,26 @@ public class Warmups {
     }
 
 
-    public static void main(String[] args) {
+    //Create an algorithm that takes in a string and returns 'true' if there are no repeating characters in the string, and false if there are repeating characters.
+        //hello     { 'h', 'e', 'l', 'l', 'o'}
+    public static String noRepeats(String a) {
 
+        for(char b : a.toCharArray()) {
+            a = a.substring(1);
+            for(char c : a.toCharArray())
+                if(b == c)
+                    return "your string contains repeating characters!";
+        }
+        return "your string does not contain repeating characters!";
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String userInput = sc.next();
+        System.out.println(noRepeats(userInput));
 
     }
 }
