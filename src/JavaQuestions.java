82b5b332084f0da84628b915bc76b5a8b41e1f70
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-12-03 09:53:13 -0600

    java questions

---
+++ src/JavaQuestions.java
@@ -0,0 +1,31 @@
+public class JavaQuestions {
+
+//    What is the difference between a .java and .class file?
+    //java file contains your Java source code while a . class file contains the Java bytecode produced by the Java compiler. It is your . class files that run on the JVM to execute a Java application.
+
+//    Why are we using an IDE (IntelliJ) instead of a simple text editor for writing Java code?
+    //compile code with JVM
+
+
+//    What is the difference between a JRE and a JDK?
+    //The JRE is the Java Runtime Environment. It is a package of everything necessary to run a compiled Java program, including the Java Virtual Machine (JVM), the Java Class Library, the java command, and other infrastructure. However, it cannot be used to create new programs.
+    //
+    //The JDK is the Java Development Kit, the full-featured SDK for Java. It has everything the JRE has, but also the compiler (javac) and tools (like javadoc and jdb). It is capable of creating and compiling programs.
+
+
+
+//    Are we currently learning about JavaSE, JavaEE, JavaME, or JavaFX?
+    //JavaSE
+
+
+//    What are some key differences between JavaScript and Java?
+    //Frontend vs Backend, Interpreted vs. Compiled,  scripting vs. programming language
+
+//    Describe in some detail the process by which a Java program is able to run on an operating system.
+    
+
+
+//    Why is Java touted as a “write once, run anywhere” language?
+
+    //Because the code runs on the cross-platform Java virtual machine, developers can write a Java program on a Windows laptop, then run it on a Linux or Solaris server without recompiling the code for the new platform
+}
